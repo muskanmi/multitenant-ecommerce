ba5ae26325faee5f574da0b880650a8411647319
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-22 22:34:01 +0530

    05: Search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilters } from "./search-filters";
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters />
       <div className="flex-1 bg-[#F4F4F0]">{children}</div>
       <Footer />
     </div>
