f4f56aa2c06d8f2148bc0187dc417a35e6d6bcb1
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-22 22:24:04 +0530

    05: Search filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -8,6 +8,14 @@ export default async function Home() {
 
   const data = await payload.find({
     collection: "categories",
+    depth: 1, // populate subcategories
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
   });
+
+  console.log(data);
   return <div>{JSON.stringify(data, null, 2)}</div>;
 }
