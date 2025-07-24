b891b6fe6c2aed195e852f2310c18cad62da8bd0
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-24 20:02:02 +0530

    05: Search filters

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -11,11 +11,15 @@ export const SearchInput = ({ disabled }: Props) => {
       <div className="relative w-full">
         <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
         <Input
-          className="p-8"
+          className="pl-8"
           placeholder="Search products"
           disabled={disabled}
         />
       </div>
+
+      {/* TODO: Add categories view all button */}
+      {/* Add library button */}
     </div>
   );
 };
+ 
