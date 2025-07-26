054cea42144e4945112352ea428c504a8632e876
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-26 23:44:16 +0530

    05: Search filters

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -27,14 +27,31 @@ export const CategoryDropdown = ({
 
   const onMouseLeave = () => setIsOpen(false);
   return (
-    <Button
-      variant="elevated"
-      className={cn(
-        "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-        isActive && !isNavigationHovered && "bg-white border-primary"
-      )}
+    <div
+      className="relative"
+      ref={dropdownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
-      {category.name}
-    </Button>
+      <div className="relative">
+        <Button
+          variant="elevated"
+          className={cn(
+            "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigationHovered && "bg-white border-primary"
+          )}
+        >
+          {category.name}
+        </Button>
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div
+            className={cn(
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
+            )}
+          ></div>
+        )}
+      </div>
+    </div>
   );
 };
