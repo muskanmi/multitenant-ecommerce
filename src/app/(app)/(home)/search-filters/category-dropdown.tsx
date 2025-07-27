2593105ae69569dfbd38e73816467e4306fa42a8
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-27 18:51:30 +0530

    05: Search filters

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -49,7 +49,7 @@ export const CategoryDropdown = ({
               "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
               isOpen && "opacity-100"
             )}
-          ></div>
+          />
         )}
       </div>
     </div>
