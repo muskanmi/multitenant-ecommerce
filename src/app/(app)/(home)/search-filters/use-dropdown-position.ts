6a122e23fcc066926f9adffcb7ac040b3c2a853f
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-27 18:57:24 +0530

    05: Search filters

---
+++ src/app/(app)/(home)/search-filters/use-dropdown-position.ts
@@ -0,0 +1,9 @@
+import { RefObject } from "react";
+
+export const useDropdownPosition = (
+  ref: RefObject<HTMLDivElement | null> | RefObject<HTMLDivElement>
+) => {
+  const getDropdownPosition = () => {};
+
+  return { getDropdownPosition };
+};
