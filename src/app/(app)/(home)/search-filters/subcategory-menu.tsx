3eec63677f4ce06f376ecd9a43004d4300b46702
Muskan Mittal <mittalmuskan758@gmail.com>
2025-07-30 13:08:45 +0530

    05: Search filters

--- src/app/(app)/(home)/search-filters/subcategory-menu.tsx
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -31,7 +31,7 @@ export const SubCategoryMenu = ({
     >
       {/* Invisible bridge to maintain hover */}
       <div className="h-3 w-60" />
-      <div className="w-60 text-black rounded-md overflow-hidden border">
+      <div className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]">
         <p>Subcategory menu</p>
       </div>
     </div>
