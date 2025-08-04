d291c9188ed55c5dda3b9e0a41dd0555f40dd68d
Muskan Mittal <mittalmuskan758@gmail.com>
2025-08-04 15:45:48 +0530

    06: categories finalization

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -1,11 +1,16 @@
 import { CategoryDropdown } from "./category-dropdown";
 import { CustomCategory } from "../types";
+import { useRef } from "react";
 
 interface CategoriesProps {
   data: CustomCategory[];
 }
 
 export const Categories = ({ data }: CategoriesProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const measureRef = useRef<HTMLDivElement>(null);
+  const viewAllRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="relative w-full">
       <div className="flex flex-nowrap items-center">
